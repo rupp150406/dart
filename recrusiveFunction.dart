187d131dda24256695b8d470e4816bb8265bce6f
rupp150406 <fuerfalien@gmail.com>
2025-02-03 16:26:44 +0700

    Update recrusiveFunction.dart

--- recrusiveFunction.dart
+++ recrusiveFunction.dart
@@ -15,7 +15,13 @@ void main() {
 
     // Input for the number
     while (true) {
+      print(" ");
+      print("=======================");
+      print("  Recrusive Function   ");
+      print("=======================");
+      print(" ");
       stdout.write("Enter a non-negative integer: ");
+      print(" ");
       String? input = stdin.readLineSync();
 
       if (input == null || input.isEmpty) {
@@ -43,13 +49,18 @@ void main() {
 
     // Ask if the user wants to calculate again
     while (true) {
+      print(" ");
       stdout.write("Do you want to calculate again? (y/n): ");
       String? repeat = stdin.readLineSync()?.toLowerCase();
 
       if (repeat == 'y') {
         break; // Exit the inner loop to repeat the calculation
       } else if (repeat == 'n') {
-        print("Thank you for using this program.");
+        print(" ");
+        print("======================================");
+        print("  Thank you for using this program.");
+        print("======================================");
+        print(" ");
         return; // Exit the program
       } else {
         print("Please enter 'y' or 'n'.");
